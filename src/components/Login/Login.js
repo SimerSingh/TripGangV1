53c78624de78b4c317f97d9395cd8a1ea3aceaf8
Varun Jyoti <Varun.jyoti@basware.com>
2018-09-30 10:13:00 +0530

    Login andn gign up page with react router

---
+++ src/components/Login/Login.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-native'
+import {  View } from 'react-native';
+import { Icon, Body, Title,  Left, Right, Button, Label, Text, Container, Header, Content, Form, Item, Input, Spinner, Grid, Col } from 'native-base';
+export default class FormExample extends Component {
+  render() {
+    return (
+        <Container>
+        <Header>
+          <Body>
+            <Title>Login</Title>
+          </Body>
+        </Header>
+        <Content>
+       <Form style={{marginTop: 100}}>
+          <Item>
+            <Input placeholder="Username" />
+          </Item>
+            <Item>
+            <Input placeholder="Password" />
+          </Item>
+           
+          <Button full rounded style={{marginTop: 10}}>
+            <Text>Login</Text>
+          </Button>
+         
+    
+        <Button full rounded success style={{marginTop: 10}}>
+             <Link to="/signup">
+            <Text>Sign up</Text></Link>
+          </Button>
+        
+        <View style={{flex: 1, flexDirection: 'row', marginTop: 10}}>
+           <Button rounded iconLeft light>
+            <Icon name='logo-googleplus' />
+            <Text>Login with G+</Text>
+          </Button>
+          <Button rounded iconLeft light>
+            <Icon name='logo-facebook' />    
+            <Text>Login with FB</Text>
+            
+          </Button>
+          </View>
+        
+          </Form>
+        </Content>
+        </Container>
+    
+    );
+  }
+}
---
+++ src/components/Login/Signup.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-native'
+import { Icon, Title, Left, Body, Button, Label, Text, Container, Header, Content, Form, Item, Input, Spinner, Grid, Col } from 'native-base';
+export default class Signup extends Component {
+  render() {
+    return (
+        <Container>
+        <Header>
+          <Left>
+            <Button transparent>
+            <Link to="/">    
+              <Icon name='arrow-back' />
+            </Link>
+            </Button>
+          </Left>
+          <Body>
+            <Title>Sign up</Title>
+          </Body>
+        </Header>
+        <Content style={{marginTop: 100}}>
+       <Form>
+        <Item>
+            <Input placeholder="Name" />
+          </Item>
+          <Item>
+            <Input placeholder="Email" />
+          </Item>
+        <Item>
+            <Input placeholder="Password" />
+          </Item>
+         <Item>
+            <Input placeholder="Phone number" />
+          </Item>
+        <Item>
+            <Input placeholder="" />
+          </Item>
+           
+          <Button full rounded>
+            <Text>Sign up</Text>
+          </Button> 
+          
+          </Form>
+        </Content>
+        </Container>
+    
+    );
+  }
+}
